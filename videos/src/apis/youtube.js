e61595081d03b51e1324e044ffc89c987c934f3a
szdziedzic <sz.dziedz@gmail.com>
2021-06-30 11:34:18 +0200

    fetch videos data and list them

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,11 +1,14 @@
-import { YOUTUBE_API_KEY } from '../../config';
+import { YOUTUBE_API_KEY } from '../config';
 import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
+    type: 'video',
     maxResults: 5,
     key: YOUTUBE_API_KEY,
   },
 });
+
+
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {};
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
