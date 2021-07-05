dd5966a29820683f03fa310a6aed234fd0f09952
szdziedzic <sz.dziedz@gmail.com>
2021-07-05 15:02:05 +0200

    basic nav from scratch

---
+++ widgets/src/components/Link.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Link = ({ href, className, children }) => {
+  const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
+    event.preventDefault();
+    window.history.pushState({}, '', href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+  };
+
+  return (
+    <a className={className} href={href} onClick={onClick}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
