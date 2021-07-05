ac26dc7d3594407702f1f3b87c57fd5722ffc5ce
szdziedzic <sz.dziedz@gmail.com>
2021-07-05 13:48:56 +0200

    navigation header

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -39,7 +41,24 @@ const App = () => {
 
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
---
+++ widgets/src/components/Route.js
@@ -0,0 +1,4 @@
+const Route = ({ path, children }) => {
+  return window.location.pathname === path ? children : null;
+};
+export default Route;
